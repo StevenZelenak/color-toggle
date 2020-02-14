f4ab592877e508902706222f987ff7144a2ca5d7
Steven Zelenak <stevenmzelenak@gmail.com>
2020-02-13 20:58:42 -0600

    it change colors

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-console.log("Hello");
+//array of color
+let colors = ["white", "red", "blue", "aqua", "orange", "yellow", "green"];
+
+//picks a random color from the array
+const randomColor = (arr) => {
+    return arr[Math.floor(Math.random() * arr.length)];
+}
+
+//when the event is called this function changes the color of the background
+const colorChange = () => {
+
+    document.body.style.backgroundColor = randomColor(colors);
+}
+
+
+const events = () => {
+    document.getElementById("color-toggle").addEventListener('click', colorChange);
+}
+
+const init = () => {
+    events();
+}
+
+init();
